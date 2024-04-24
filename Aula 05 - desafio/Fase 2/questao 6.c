ef65c9a9d2bdccfb4dd35a159b2f7fd2c118076c
cecilia-ionara <128868521+cecilia-ionara@users.noreply.github.com>
2024-04-24 14:46:57 -0300

    Add files via upload

--- Aula 05 - desafio/Fase 2/questao 6.c
+++ Aula 05 - desafio/Fase 2/questao 6.c
@@ -1,25 +1,19 @@
 
 #include <stdio.h>
-#include <string.h>
 
-char letra[1];
+char letra;
 
-int main ()
-{
-  printf ("Olá!!, insira a letra A:");
-  scanf ("%s", letra);
+int main () {
+	printf ("Ola!!, insira a letra:");
+	scanf ("%c", &letra);
 
-  if (strcmp (letra, "a") == 0)
-	{
-	  printf ("A letra é minuscula");
+	if (letra >= 'a' && letra <= 'z') {
+		printf("A letra digitada eh uma letra minuscula.\n");
+	} else if (letra >= 'A' && letra <= 'Z') {
+		printf("A letra digitada eh uma letra maiscula.\n");
+	} else {
+		printf("Nao foi digitada uma letra.\n");
 	}
-  else if (strcmp (letra, "A") == 0)
-	{
-	  printf ("A letra é maiscula");
-	}
-  else
-	{
-	  printf ("Por favor, insira a letra A");
-	}
-  return 0;
+
+	return 0;
 }
