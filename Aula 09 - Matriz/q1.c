ec69d8f3c4911083644a719d569091175b16e1e1
cecilia-ionara <128868521+cecilia-ionara@users.noreply.github.com>
2024-08-22 09:01:41 -0300

    Add files via upload

--- Aula 09 - Matriz/q1.c
+++ Aula 09 - Matriz/q1.c
@@ -1,27 +1,33 @@
 /******************************************************************************
-
+MATRIZ
 *******************************************************************************/
 #include <stdio.h>
-
+#include<locale.h>
 int main()
 {
-    int i, j, k, matriz[10][10], valor;
+	setlocale(LC_ALL,"");
+    int i, j, k, matriz[3][3], valor, soma=0;
     printf("Aula Matriz\n");
     for(i=0;i<3;i++){
-        printf("\n\n%iÂ° linha: \n", i+1);
+    	//recebe linhas (o usuario nao precisa inserir pq vai ser sempre em ordem
+        printf("\n%i° linha: \n", i+1);
         for(j=0;j<3;j++){
-            printf("%iÂ° coluna: ", j+1);
+		//usuario insere o valor q ta naquela linha e coluna
+            printf("%i° coluna: ", j+1);
             scanf("%i", &matriz[i][j]);
+            soma = soma + matriz[i][j];
         }
     }
-    
+    printf("\nResultado: \n");
     for(i=0;i<3;i++){
         printf("[ ");
         for(j=0;j<3;j++){
+        	//exibindo a linha de acordo com coluna
             printf(" %i ", matriz[i][j]);
         }
         printf(" ]\n");
     }
     
+    printf("\nA soma de todos os elementos da matriz é %i", soma);
     return 0;
 }
