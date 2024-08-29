55176667a2c9fdab028096588e038c182a34de1d
cecilia-ionara <128868521+cecilia-ionara@users.noreply.github.com>
2024-08-29 09:57:45 -0300

    Add files via upload

--- Aula 11 - Ponteiro/questao 1 - executando.cpp
+++ Aula 11 - Ponteiro/questao 1 - executando.cpp
@@ -20,7 +20,7 @@ int main(){
 	printf("\nP2 (endereco guardado de p1(que eh o de num)): %i", p2);
 	printf("\n*P1 (conteudo de num): %i", *p1);
 	printf("\n*P2 (conteudo de p1): %i", *p2);
-	printf("\n&P1 (endereco original): %i", &p1);
+	printf("\n&P1 (endereco original): %i", &p1); 
 	printf("\n&P2 (endereco original): %i", &p2);	
 	
 	return 0;
