8dd1abd54e24dad7ce604d840b71f7236df4caee
cecilia-ionara <128868521+cecilia-ionara@users.noreply.github.com>
2024-09-03 10:23:06 -0300

    Add files via upload

---
+++ Aula 11 - Ponteiro/troca funcao.cpp
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+void troca(int *a, int *b) {
+    int c;
+    c = *a;
+    *a = *b;
+    *b = c;
+}
+
+int main() {
+    int num1, num2;
+
+    printf("Num 1: ");
+    scanf("%i", &num1);
+    printf("Num 2: ");
+    scanf("%i", &num2);
+
+    troca(&num1, &num2);//manda endereço da variavel
+
+    printf("\nTroca\nNum 1: %i\nNum 2: %i", num1, num2);
+    return 0;
+}
+
