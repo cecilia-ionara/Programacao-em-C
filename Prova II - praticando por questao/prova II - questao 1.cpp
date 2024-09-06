b3215663f6cc51ce4cc7451c2cfca0527c75722c
cecilia-ionara <128868521+cecilia-ionara@users.noreply.github.com>
2024-09-06 10:59:35 -0300

    Add files via upload

---
+++ Prova II - praticando por questao/prova II - questao 1.cpp
@@ -0,0 +1,16 @@
+/*1. (Vale 5 pontos) Crie uma matriz de número inteiros, de ordem 4. Preencha os valores
+ dessa matriz com valores acima de 100 (podem ser números sequenciais, se preferir). 
+ Em seguida, faça o que se pede: 
+
+a) Imprima todos os elementos da matriz;
+b) Imprima os valores da diagonal secundária e, em seguida, imprima o produto dos 
+elementos dessa diagonal;
+c) Calcule e imprima a soma dos elementos da matriz, onde o índice da coluna é igual a 2;
+d) Calcule e imprima o produto dos elementos da matriz, onde o índice da linha é igual a 0;
+*/
+//treinar função
+#include<stdio.h>
+
+int main(){
+	return 0;
+}
---
+++ Prova II - praticando por questao/prova II - questao 2.cpp
@@ -0,0 +1,10 @@
+/* 2. (Vale 5 pontos) Crie um vetor de tamanho 10, do tipo float e, em seguida, utilizando passagem de parâmetros por referência, faça o que se pede a seguir:
+a) Crie uma função para ler 10 valores informados pelo usuário. Esses valores irão preencher o vetor;
+b) Crie uma função para imprimir o conteúdo do vetor e, também, uma outra função para calcular a soma dos elementos desse vetor;
+c) Execute os seguintes passos: 
+i) chame a função de leitura do vetor e preencha-os com quaisquer números;
+ii) chame a função para imprimir esse vetor;
+iii) chame a função que calcula a soma dos elementos do vetor, e imprima o resultado dessa soma; 
+iv) altere o conteúdo do vetor em que o índice seja igual a 9; 
+v) chame a função para imprimir esse vetor;
+vi) chame a função que calcula a soma dos elementos do vetor, e imprima o resultado dessa soma; */
